25babf9e72dda2d3bffdbf685d227f73c08b940c
watercubz <watercubz>
2023-05-20 14:10:27 -0400

    update

--- qwik-app/src/routes/pokemon/index.tsx
+++ qwik-app/src/routes/pokemon/index.tsx
@@ -10,34 +10,39 @@ export default component$(() => {
   });
   return (
     <>
-      <div class=" bg-gray-600 text-center text-white">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Store.value}.png`}
-          alt="pokemon"
-          style={{ width: "200px" }}
-        />
-
-        <span>{Store}</span>
-      </div>
-
-      <div class="text-center gap-5 mt-3">
-        <button
-          onClick$={() => changeStore(+1)}
-          class="btn btn-active gap-14 text-center py-4 px-3"
-        >
-          siguiente:
-        </button>
-        <div class="p-9">
-          <button
-            onClick$={() => changeStore(-1)}
-            class="btn btn-active py-4 px-3"
-          >
-            anterior:
-          </button>
+      <div class="card w-96 bg-gray-600 shadow-xl text-white p-10 mt-8 m-36">
+        <div class="card-body">
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Store.value}.png`}
+            alt="pokemon"
+            style={{ width: "300px" }}
+          />
+          <p class="text-center">Click to see your favorite pokemon!</p>
         </div>
-      </div>
-      <div class="text-center mt-6">
-        <Link href="/">Home</Link>
+        <div class="text-center gap-5 mt-3">
+          <button
+            onClick$={() => changeStore(+1)}
+            class="btn btn-active gap-14 text-center py-4 px-3"
+          >
+            siguiente:
+          </button>
+          <div class="p-9">
+            <button
+              onClick$={() => changeStore(-1)}
+              class="btn btn-active py-4 px-3"
+            >
+              anterior:
+            </button>
+          </div>
+        </div>
+
+        <div class="text-center">
+          <span>{Store}</span>
+        </div>
+
+        <div class="text-center p-3">
+          <Link href="/">Home</Link>
+        </div>
       </div>
     </>
   );
