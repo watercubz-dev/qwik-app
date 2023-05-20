aa6b7a220543c81802fec7ee6e6004a8ae6c860e
watercubz <watercubz>
2023-05-20 13:43:57 -0400

    update qwik app

--- qwik-app/src/routes/about/index.tsx
+++ qwik-app/src/routes/about/index.tsx
@@ -3,13 +3,13 @@ import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <di class="text-center py-5">
-      <h1 class="text-center bg-gray-500">welcome to about</h1>
-      <button class="text-center py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded">
-        <Link href="/" class="mt-">
-          home
-        </Link>
-      </button>
-    </di>
+    <>
+      <div>
+        <h1 class="text-center bg-gray-600 p-5 text-white">en el about</h1>
+      </div>
+      <div class="text-center mt-8">
+      <Link href="/">ir a Home</Link>
+      </div>
+    </>
   );
 });
--- qwik-app/src/routes/pokemon/index.tsx
+++ qwik-app/src/routes/pokemon/index.tsx
@@ -1,11 +1,44 @@
-import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { $, component$, useSignal, useStore } from "@builder.io/qwik";
+import { Link, type DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return <div>
-        <span>
-            <h1>poken api </h1>
-            <Link href="/" class="text-center">Home</Link>
-        </span>
-  </div>;
+  const Store = useSignal(1);
+  const changeStore = $((value: number) => {
+    if (Store.value + value <= 0) return;
+
+    Store.value += value;
+  });
+  return (
+    <>
+      <div class=" bg-gray-600 text-center text-white">
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${Store.value}.png`}
+          alt="pokemon"
+          style={{ width: "200px" }}
+        />
+
+        <span>{Store}</span>
+      </div>
+
+      <div class="text-center gap-5 mt-3">
+        <button
+          onClick$={() => changeStore(+1)}
+          class="btn btn-active gap-14 text-center py-4 px-3"
+        >
+          siguiente:
+        </button>
+        <div class="p-9">
+          <button
+            onClick$={() => changeStore(-1)}
+            class="btn btn-active py-4 px-3"
+          >
+            anterior:
+          </button>
+        </div>
+      </div>
+      <div class="text-center mt-6">
+        <Link href="/">Home</Link>
+      </div>
+    </>
+  );
 });
